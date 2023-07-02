b1e07eb3e84e7d827de2d7584cbf46fcbb8b8ff1
thien <nguyenthanhthien0507@gmail.com>
2023-07-02 22:46:06 +0700

    update test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,41 @@ export class AppComponent {
         xfbml: true,
         version: 'v17.0',
       });
+
       FB.getLoginStatus((response: any) => {
         statusChangeCallback(response);
       });
 
       FB.AppEvents.logPageView();
+
+      FB.api('/me/accounts', function (response: any) {
+        if (response && !response.error) {
+          console.log(response); // Danh sách các trang của người dùng
+          // Thực hiện xử lý với danh sách trang ở đây
+        }
+      });
     };
 
     function statusChangeCallback(response: any) {
+      console.log('statusChangeCallback');
       console.log(response);
+      // The response object is returned with a status field that lets the
+      // app know the current login status of the person.
+      // Full docs on the response object can be found in the documentation
+      // for FB.getLoginStatus().
+      if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function (response: any) {
+          console.log('Successful login for: ' + response.name);
+          document.getElementById('status')!.innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+        });
+      } else {
+        // The person is not logged into your app or we are unable to tell.
+        document.getElementById('status')!.innerHTML =
+          'Please log ' + 'into this app.';
+      }
     }
 
     (function (d, s, id) {
